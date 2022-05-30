d54dddad3e2ba757db921ff3e93e1ab3faae2adf
jusa1470 <sanfordjj1997@gmail.com>
2022-05-30 13:34:22 -0600

    wrote tests for player and added more setup commands (#4)

--- app/src/Player.py
+++ app/src/Player.py
@@ -1,4 +1,4 @@
-from app.enums.colorEnums import ColorEnums
+from ...app.enums.colorEnums import ColorEnums
 
 class Player:
     def __init__(self, name = "", color = ColorEnums.NONE):
@@ -53,17 +53,23 @@ class Player:
         self.treasure.append(tile)
 
     def addTreasureToVault(self):
-        self.vault.append(self.treasure)
+        self.vault.extend(self.treasure)
         self.removeAllTreasure()
 
     def removeTreasure(self, tile):
-        self.treasure.remove(tile)
+        idx = -1
+        for i, t in enumerate(self.treasure):
+            if tile.getDots() == t.getDots() and tile.getValue() == t.getValue():
+                idx = i
+                break
+        if idx != -1:
+            del self.treasure[idx]
 
     def removeAllTreasure(self):
         self.treasure.clear()
 
     def getLowestTreasure(self):
-        sortedTreasure = self.treasure.sort(key=lambda tile: tile.getDots())
-        lowestTreasure = sortedTreasure[0]
+        self.treasure.sort(key=lambda tile: tile.getDots())
+        lowestTreasure = self.treasure[0]
         self.treasure.remove(lowestTreasure)
         return lowestTreasure
